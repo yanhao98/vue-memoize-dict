b6b06b7cc7f7723813b26298ec48c443a46f24b5
严浩 <me@yanhao.ren>
2023-12-19 10:32:23 +0800

    0.0.19

--- packages/vue-memoize-dict/rollup.config.mjs
+++ packages/vue-memoize-dict/rollup.config.mjs
@@ -1,18 +1,16 @@
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
-// import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" assert { type: "json" };
 
 const config = defineConfig({
   input: "src/index.ts",
   output: [
     {
-      // file: pkg.main,
-      file: "./dist/index.cjs",
+      file: pkg.main,
       format: "cjs",
     },
     {
-      // file: pkg.module,
-      file: "./dist/index.mjs",
+      file: pkg.module,
       format: "es",
     },
   ],

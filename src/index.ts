4e7e74de8ed22434a3ccbb805011c34c7c511f5e
严浩 <me@yanhao.ren>
2024-01-25 11:08:35 +0800

    1.0.2-beta.2: 优化。

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
 // https://github.com/yanhao98/vue-memoize-dict/blob/a1f73b60b0b51823ffb3dc5ed4606f3f8d0bb644/packages/playground/src/components/MemoizeDict.vue
+// https://github.dev/vueuse/vueuse/blob/main/packages/core/useMemoize/index.ts
+
 import { computedAsync, useMemoize } from "@vueuse/core";
-
-type AsyncComputedReturnType<T> = ReturnType<typeof computedAsync<T>>;
-
 export class MemoizeDict<DictItem = Record<string, unknown>> {
   private options;
   private memoFetch;
-  private computedMap: Map<string, AsyncComputedReturnType<DictItem[] | undefined>> = new Map();
 
   constructor(options: DatasetOptions<DictItem>) {
     this.options = options;
-    this.memoFetch = useMemoize(this._fetch.bind(this));
+    this.memoFetch = useMemoize(
+      this._fetch.bind(this),
+      {
+        getKey: (dictName: string) => dictName,
+      }
+    );
   }
 
-  public get(dictName: string) {
-    if (!this.computedMap.has(dictName)) {
-      const asyncComputedRef = computedAsync(() => {
-        return this.memoFetch(dictName);
-      });
-      this.computedMap.set(dictName, asyncComputedRef);
-    }
-    const computedRef = this.computedMap.get(dictName)!;
-    return computedRef.value;
+  public get(dictName: string): DictItem[] | undefined {
+    if (!this.memoFetch.cache.has(dictName))
+      this.memoFetch(dictName);
+
+    const cachedPromise = this.memoFetch.cache.get(dictName)!;
+    cachedPromise!.__computedAsyncRef__ ??= computedAsync(() => cachedPromise);
+    return cachedPromise!.__computedAsyncRef__.value;
   }
 
   /**
-   * Call memoized function
+   * Get result from cache or call memoized function
    */
   public fetch(dictName: string) {
     return this.memoFetch(dictName);
@@ -83,12 +84,17 @@ export class MemoizeDict<DictItem = Record<string, unknown>> {
     return fullLabel
   }
 
-  private async _fetch(dictName: string): Promise<DictItem[]> {
+  private _fetch(dictName: string): Promise<DictItem[]> & { __computedAsyncRef__?: ReturnType<typeof computedAsync<DictItem[]>> } {
     const config = this.options.config[dictName];
     if (!config) {
       throw new Error(`config not found: ${dictName}`);
     }
-    return (await config.data()) as DictItem[];
+    let promise = config.data() as ReturnType<typeof this._fetch>;
+    if (!(promise instanceof Promise)) {
+      promise = Promise.resolve(promise);
+    }
+    promise.__computedAsyncRef__ = undefined;
+    return promise;
   }
   private get _valueFieldName() {
     return (this.options.fieldNames?.value || "value") as keyof DictItem;
@@ -106,12 +112,11 @@ interface FieldNamesConfig<DictItem> {
   parent?: keyof DictItem;
 }
 
-// type DictArray<T> = Array<T>;
-
 interface DatasetConfig<DictItem = Record<string, unknown>> {
-  data(/* params?: Recordable<unknown> */): Promise<DictItem[]>;
+  data(/* params?: Recordable<unknown> */): Promise<DictItem[]> | DictItem[];
 }
 interface DatasetOptions<DictItem> {
   config: Record<string, DatasetConfig<DictItem> | undefined>;
   fieldNames?: FieldNamesConfig<DictItem>;
 }
+

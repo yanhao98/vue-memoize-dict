8b1bfdfd79188e7d2ab05fbd84a49a8c1ae8f3c2
oo1dev <me@yanhao.ren>
2024-04-01 22:12:43 +0800

    Update URLs and comments in Playwright tests

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://vue-memoize-dict-git-dependabot-npman-e1a7f9-yanhao98s-projects.vercel.app/');
+  await page.goto('/');
   await page.waitForLoadState('domcontentloaded');
   await page.evaluate(async () => window.remoteDict.fetch('food'))
 });
